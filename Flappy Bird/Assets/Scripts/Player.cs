00e3ab628fb453bc1ecabf8a92733b14cc7d5f72
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2020-12-29 18:54:45 -0300

    Versão 1.0
    Versão 1.0

---
+++ Flappy Bird/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed;
+    private Rigidbody2D rig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))//0: esquerdo; 1: direito;
+        {
+            rig.velocity = Vector2.up * Speed;
+        }
+
+    }
+}
